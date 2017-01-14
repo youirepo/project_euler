4c313f6f929b7d03d628564628e4a35536362156
Yousif Solaiman <Yousif.Solaiman@dolby.com>
2017-01-14 12:39:27 +1100

    Initial solution was wrong.
    If statement tested wrong condition. Evaluated to true when
    a number was not a multiple of 3 or 5. This has been fixed to
    evaluate to true when it is a multiple of 3 or 5.

--- Python/problem_01.py
+++ Python/problem_01.py
@@ -1,9 +1,16 @@
-import os
-import sys
+"""Multiples of 3 and 5
+
+Problem 1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 
 summ = 0
-for n in xrange(3,1000):
-	if n%3 or n%5:
+for n in xrange(3, 1000):
+	if not n%3 or not n%5:
 		summ += n
-
+	
 print summ
